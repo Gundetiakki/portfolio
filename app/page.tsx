d1e4e473df2681cd809599fd9d798d74e728b3d7
aakash.g <my@work>
2023-11-10 00:24:29 +0530

    wording change

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <h1 className="font-bold text-teal-500 text-2xl">
-      Welcome to Aakash!!
+      Welcome to Aakash's Website!!
     </h1>
   )
 }
